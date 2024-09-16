bfaad962a72de2db8ed4bded020252165455db1c
facundomora2005@gmail.com <tomasvalverde161@gmail.com>
2024-09-16 20:37:31 -0300

    este es mi primer push

---
+++ src/main/java/ejemplo/repositorio/Repositorio.java
@@ -0,0 +1,42 @@
+
+
+package ejemplo.repositorio;
+
+
+
+public class Repositorio {
+
+    public static void main(String[] args) {
+        try {
+            int[] numeros = {1,2,3} ; 
+            System.out.println("accediento al cuaRTO ELEMENTO "+ numeros[3]);
+        } catch (ArrayIndexOutOfBoundsExceptionay e) {
+             System.out.println("ERROR: inidice fuera de los limites de arreglo");
+        }
+        System.out.println("fin del programa ");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
